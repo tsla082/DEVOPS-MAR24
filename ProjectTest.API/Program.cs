dced307772abe717b2c66e47208a68f7152f1597
tsla082 <torresluis082@gmail.com>
2024-03-08 21:22:55 -0500

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,13 +11,13 @@ var app = builder.Build();
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 15122023-v1.0.0 CI/CD PROJECT FREE STYLE JENKINS JOB   v1.0.2 POLYGLOT STUDY";
+    return "GET - Net 6 Running ... 15122023-v1.0.0 CI/CD PROJECT FREE STYLE JENKINS JOB   v1.0.2 POLYGLOT STUDY *";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "POST - Net 6 Running ...15092023-v1.0.0 CI/CD in AKS v1.0.0 POLYGLOT";
+    return "POST - Net 6 Running ...15092023-v1.0.0 CI/CD in AKS v1.0.0 POLYGLOT ** ";
 });
 
 //app.Run($"http://0.0.0.0:{port}");
